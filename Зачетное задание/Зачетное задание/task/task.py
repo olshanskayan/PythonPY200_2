b802bf698779afb0494e6c9f308b22c0ba5a68b8
natols <olshanskaya@mail.ru>
2021-09-23 21:21:47 +0300

    exams

--- Зачетное задание/Зачетное задание/task/task.py
+++ Зачетное задание/Зачетное задание/task/task.py
@@ -3,11 +3,14 @@ from typing import Any, Optional
 
 class Node:
     """ Класс, который описывает узел связного списка. """
+#    def __init__(self):
     ...
 
 
 class DoubleLinkedNode(Node):
-    ...
+    def __init__(self):
+        super().__init__(self)
+        self.prev = self.value
 
 
 if __name__ == "__main__":
