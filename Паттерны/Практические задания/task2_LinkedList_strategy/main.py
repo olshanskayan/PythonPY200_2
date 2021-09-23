1423b1540955c81cab6c5e52eaee46adced240f1
natols <olshanskaya@mail.ru>
2021-09-23 20:00:52 +0300

    pattern

--- Паттерны/Практические задания/task2_LinkedList_strategy/main.py
+++ Паттерны/Практические задания/task2_LinkedList_strategy/main.py
@@ -5,22 +5,39 @@ from drivers import IStructureDriver
 from factory_method import SimpleFileFactoryMethod
 
 
-class LinkedListWithDriver(...):  # TODO наследовать класс LinkedList
+class LinkedListWithDriver(LinkedList):  # наследовать класс LinkedList
     def __init__(self, data: Iterable = None, driver: IStructureDriver = None):
-        ...  # TODO расширяем конструктор, чтобы в связном списке был driver
+        #расширяем конструктор, чтобы в связном списке был driver
+        super().__init__(data)
+        self.driver = driver
 
     def read(self):
         """ С помощью драйвера считать данные и поместить их в LinkedList. """
-        ...  # TODO считать данные из драйвера
+        #считать данные из драйвера
+        self.clear()
+        data = self.driver.read()
+
+        for item in data:
+            self.append(item)
 
     def write(self):
         """ С помощью драйвера записать данные из LinkedList. """
-        ...  # TODO записать данные с помощью драйвера
+        #записать данные с помощью драйвера
+        self.driver.write(self)
 
 
 if __name__ == '__main__':
     ll = LinkedListWithDriver([1, 2, 3, 4, 5])
-    # TODO инициализировать драйвер и записать данные
+    #инициализировать драйвер и записать данные
+    driver_1 = SimpleFileFactoryMethod.get_driver()
 
-    # TODO заменить драйвер и считать данные
+    ll.driver = driver_1
+
+    ll.write()
+
+    #заменить драйвер и считать данные
+
+    driver_2 = SimpleFileFactoryMethod.get_driver()
+    ll.driver = driver_2
+    ll.read()
     print(ll)
