7b9fe3a85d9cd02fc808ef4322ee8105c4ee61f3
Наталья <olshanskaya@mail.ru>
2021-09-30 13:44:19 +0300

    exams

--- Атрибуты и методы/Повторение/task6_Glass__dict__/main.py
+++ Атрибуты и методы/Повторение/task6_Glass__dict__/main.py
@@ -1,8 +1,9 @@
 class Glass:
     def __init__(self, capacity_volume: [int, float], occupied_volume: [int, float]):
+        print(self.__dict__)
         self.capacity_volume = capacity_volume  # объем стакана
         self.occupied_volume = occupied_volume  # объем жидкости в стакане
-
+        print(self.__dict__)
 
 if __name__ == "__main__":
     glass = Glass(200, 100)
